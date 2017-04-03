7907fff5404189b2d220dc878a6987b2864a3a2b
Konrad <226931@student.pwr.edu.pl>
2017-04-03 21:36:01 +0100

    hehe

--- Testownik/AddingPanel.cs
+++ Testownik/AddingPanel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,10 +17,13 @@ namespace Testownik
         //zmienne
         public string NameOfQuizDeliver { get; set; }
         private int quizID { get; set; }
+        private List<Question> questList { get; set; }
+        private List<Question> qlist = new List<Question>();
 
         public AddingPanel()
         {
             InitializeComponent();
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
         private void AddingPanel_Load(object sender, EventArgs e)
         {
@@ -41,86 +45,94 @@ namespace Testownik
         {
             if (Add_button.Text == "Dodaj")
             {
-                using (var db = new AppContext())
+                if (isChecked() == true)
                 {
-                    Quiz quiz= new Quiz();
-                    quiz = db.Quizes.FirstOrDefault(a => a.Id == quizID);
+                    using (var db = new AppContext())
+                    {
+                        Quiz quiz = new Quiz();
+                        quiz = db.Quizes.FirstOrDefault(a => a.Id == quizID);
 
-                    quiz.Questions= new List<Question>();
-                    var answers= new List<Answer>();    //tworzy sie lista ktora potem jest wrzucana do bazy
-                    Question question = new Question()
-                    {
-                        Ask = Question_textbox.Text,
-                        Answers = answers  //tutaj se tą liste wrzuca
-                    };
+                        quiz.Questions = new List<Question>();
+                        var answers = new List<Answer>(); //tworzy sie lista ktora potem jest wrzucana do bazy
+                        Question question = new Question()
+                        {
+                            Ask = Question_textbox.Text,
+                            Answers = answers //tutaj se tą liste wrzuca
+                        };
 
-                    if (A_checkbox.Checked)   //dodajemy 1 odp do bazy
-                    {
-                        answers.Add(new Answer()
+                        if (A_checkbox.Checked) //dodajemy 1 odp do bazy
                         {
-                            Ans = A_textbox.Text,
-                            IsCorrect = true
-                        });
-                    }
-                    else 
-                    {
-                        answers.Add(new Answer()
+                            answers.Add(new Answer()
+                            {
+                                Ans = A_textbox.Text,
+                                IsCorrect = true
+                            });
+                        }
+                        else
                         {
-                            Ans = A_textbox.Text
-                        });
-                    }
+                            answers.Add(new Answer()
+                            {
+                                Ans = A_textbox.Text
+                            });
+                        }
 
-                    if (B_checkbox.Checked)      //dodajemy 2 odp
-                    {
-                        answers.Add(new Answer()
+                        if (B_checkbox.Checked) //dodajemy 2 odp
                         {
-                            Ans = B_textbox.Text,
-                            IsCorrect = true
-                        });
-                    }
-                    else
-                    {
-                        answers.Add(new Answer()
+                            answers.Add(new Answer()
+                            {
+                                Ans = B_textbox.Text,
+                                IsCorrect = true
+                            });
+                        }
+                        else
                         {
-                            Ans= B_textbox.Text
-                        });
-                    }
+                            answers.Add(new Answer()
+                            {
+                                Ans = B_textbox.Text
+                            });
+                        }
 
-                    if (C_checkbox.Checked)        //dodajemy 3 odp
-                    {
-                        answers.Add(new Answer()
+                        if (C_checkbox.Checked) //dodajemy 3 odp
                         {
-                            Ans = C_textbox.Text,
-                            IsCorrect = true
-                        });
-                    }
-                    else
-                    {
-                        answers.Add(new Answer()
+                            answers.Add(new Answer()
+                            {
+                                Ans = C_textbox.Text,
+                                IsCorrect = true
+                            });
+                        }
+                        else
                         {
-                            Ans = C_textbox.Text
-                        });
-                    }
+                            answers.Add(new Answer()
+                            {
+                                Ans = C_textbox.Text
+                            });
+                        }
 
-                    if (D_checkbox.Checked)       //dodajemy 4 odp
-                    {
-                        answers.Add(new Answer()
+                        if (D_checkbox.Checked) //dodajemy 4 odp
                         {
-                            Ans = D_textbox.Text,
-                            IsCorrect = true
-                        });
-                    }
-                    else
-                    {
-                        answers.Add(new Answer()
+                            answers.Add(new Answer()
+                            {
+                                Ans = D_textbox.Text,
+                                IsCorrect = true
+                            });
+                        }
+                        else
                         {
-                            Ans = D_textbox.Text
-                        });
+                            answers.Add(new Answer()
+                            {
+                                Ans = D_textbox.Text
+                            });
+                        }
+                        quiz.Questions.Add(question); //tutaj zostala dodana lista ze wszystkimi odp
+                        db.SaveChanges();
+                        questList = quiz.Questions;
+                        updateDGV();
+                        
                     }
-                     quiz.Questions.Add(question);     //tutaj zostala dodana lista ze wszystkimi odp
-                    db.SaveChanges();
+                    clearFields();
+                    MessageBox.Show("Pomyślnie dodano pytanie");
                 }
-                clearFields();
+                
             }
         }
 
@@ -137,6 +149,46 @@ namespace Testownik
             D_textbox.Clear();
             Question_textbox.Clear();
 
+        }
+
+        private void updateDGV()
+        {
+            qlist.AddRange(questList);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = qlist;
+            dataGridView1.Columns["Id"].HeaderText = "Id pytania";
+            dataGridView1.Columns["Ask"].HeaderText = "Treść pytania";
+            dataGridView1.Columns["Quiz"].Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        private bool isChecked()
+        {
+            if (string.IsNullOrEmpty(A_textbox.Text) || string.IsNullOrEmpty(B_textbox.Text) ||
+                string.IsNullOrEmpty(C_textbox.Text) || string.IsNullOrEmpty(D_textbox.Text) ||
+                string.IsNullOrEmpty(Question_textbox.Text))
+            {
+                MessageBox.Show("Błąd: Nie wypełniłeś wszystkich pól!");
+                return false;
+            }
+            else
+            {
+                if (A_checkbox.Checked==false && B_checkbox.Checked==false && C_checkbox.Checked==false && D_checkbox.Checked==false)
+                {
+                    MessageBox.Show(
+                        "Błąd: Nie zaznaczyłeś ani jednej poprawnej odpowiedzi. Zaznacz przynajmniej jedną!");
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+       private void getAnswers()
+        {
+            
         }
     }
 }
